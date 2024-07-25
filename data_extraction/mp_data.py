662484eb2a580f4aba6e1796c7a3d8c48d77e538
Will Ondrik <wondrik@bcit.ca>
2024-07-25 09:12:30 -0700

    Added a function to extract MP data: - pulls the first and last name, constituency, caucus, and salary before appending them to the MP dictionary

---
+++ data_extraction/mp_data.py
@@ -0,0 +1,25 @@
+from data_extraction.utils import format_dollar_values
+
+def extract_mp_data(row_data):
+    mp_data = []
+    name = row_data[0].text.strip().split(', ')
+    
+    if name[0] == 'Vacant':
+        first_name = name[0].strip()
+        last_name = name[0]
+    else:
+        first_name = name[1].strip()
+        last_name = name[0]
+    
+    constituency = row_data[1].text.strip()
+    caucus = row_data[2].text.strip()
+    salary = format_dollar_values(row_data[3].text.strip())
+    mp_data.append({
+        'first_name': first_name,
+        'last_name': last_name,
+        'constituency': constituency,
+        'caucus': caucus,
+        'salary': salary
+    })
+
+    return mp_data
