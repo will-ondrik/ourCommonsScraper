5b15bcd501ecef46628f22ef36b5a403d43570b3
Will Ondrik <wondrik@bcit.ca>
2024-07-25 12:51:10 -0700

    Corrected error. I wasn't setting the expenseId so insertions relying on this value were previously unsuccessful.

--- data_processing/process_contract_expenses.py
+++ data_processing/process_contract_expenses.py
@@ -11,6 +11,7 @@ def sequelize(contracts_data, startPeriod, endPeriod, year, quarter):
     else:
         for data in contracts_data:
             script.append(utils.expense_insert('contract', data['total_cost'], startPeriod, endPeriod, year, quarter))
+            script.append(utils.set_var_expense_id())
             script.append(contract_claim_insert(data['supplier'], data['date'], data['description'], data['total_cost']))
 
     return script
