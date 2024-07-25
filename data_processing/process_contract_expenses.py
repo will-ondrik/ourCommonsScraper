a17c3a89b8f619e025d242a6be4318f5ed436a58
Will Ondrik <wondrik@bcit.ca>
2024-07-25 09:37:39 -0700

    Created 2 functions:
    1) contract claim insert which:
    - returns an f-string that contains all contract expense related data.
    
    2) sequelize which:
    - creates an empty list to hold the contract expense data.
    - iterates through the data and appends an expense insert (which also sets the expenseId for following expenses to reference).
    - appends a contract claim.

---
+++ data_processing/process_contract_expenses.py
@@ -0,0 +1,16 @@
+import data_processing.utils as utils
+
+def contract_claim_insert(supplier, date, purpose, total_cost):
+    return f"INSERT INTO ContractClaim (expenseId, memberId, supplier, date, purpose, totalCost) VALUES (@expenseId, @mpId, '{supplier}', '{date}', '{purpose}', {total_cost});"
+
+def sequelize(contracts_data, startPeriod, endPeriod, year, quarter):
+    script = []
+
+    if not contracts_data:
+        return
+    else:
+        for data in contracts_data:
+            script.append(utils.expense_insert('contract', data['total_cost'], startPeriod, endPeriod, year, quarter))
+            script.append(contract_claim_insert(data['supplier'], data['date'], data['description'], data['total_cost']))
+
+    return script
