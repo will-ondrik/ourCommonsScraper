fb5ca91c8ebb6ed2ab611d929c95c49124267485
Will Ondrik <wondrik@bcit.ca>
2024-07-25 09:08:08 -0700

    Added an extract contract expenses function which: - receives the webdriver and href value as arguments - adds the href value to the page url constant to create the correct url for the page - uses the driver and beautiful soup to extract and parse the html - iterates through the data and appends objects to the contract data dictionary

---
+++ data_extraction/contract_expenses.py
@@ -0,0 +1,44 @@
+from bs4 import BeautifulSoup
+from constants.constants import BASE_URL, NO_TBODY_FOUND, EXPENSES_MAIN_INFO
+from data_extraction.utils import format_dollar_values
+import time
+
+
+def extract_contract_expenses(driver, href_value):
+    time.sleep(1)
+    contract_data = []
+    PAGE_URL = BASE_URL + href_value
+    driver.get(PAGE_URL)
+    time.sleep(1)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    tbody = soup.find('tbody')
+    if not tbody:
+        print(NO_TBODY_FOUND)
+        return
+    
+
+    rows = tbody.find_all('tr')
+    i = 0
+    while i < len(rows):
+        row = rows[i]
+        if EXPENSES_MAIN_INFO in row.get('class', []):
+            row_data = row.find_all('td')
+            supplier = row_data[0].text.strip()
+            description = row_data[1].text.strip()
+            date = row_data[2].text.strip()
+            total_cost = format_dollar_values(row_data[3].text.strip())
+
+            contract_data.append({
+                'supplier': supplier,
+                'description': description,
+                'date': date,
+                'total_cost': total_cost
+            })
+        i += 1
+
+    return contract_data
+        
+    
