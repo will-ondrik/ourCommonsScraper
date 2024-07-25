fdcb5ff6aca188c65050551cce07a15fe1b39778
Will Ondrik <wondrik@bcit.ca>
2024-07-25 12:51:16 -0700

    Corrected error. I wasn't setting the expenseId so insertions relying on this value were previously unsuccessful.

--- data_processing/process_hospitality_expenses.py
+++ data_processing/process_hospitality_expenses.py
@@ -7,13 +7,13 @@ def event_insert(claim_number, event_type, supplier, cost):
     return f"INSERT INTO event (claimId, claimNumber, type, purpose, supplier, totalCost) VALUES (@claimId, '{claim_number}', '{event_type}', '{supplier}', {cost});"
 
 def sequelize(hospitality_data, startPeriod, endPeriod, year, quarter):
-    print('sqlized hospitality', hospitality_data)
     script = []
     if not hospitality_data:
         return
     else:
         for data in hospitality_data:
             script.append(utils.expense_insert('hospitality', data['total_cost'], startPeriod, endPeriod, year, quarter))
+            script.append(utils.set_var_expense_id())
             script.append(hospitality_claim_insert(data['date'], data['location'], data['attendance'], data['purpose'], data['total_cost']))
             script.append(utils.set_var_claim_id())
 
@@ -21,7 +21,6 @@ def sequelize(hospitality_data, startPeriod, endPeriod, year, quarter):
                 continue
             else:
                 for nested_data in data['details']:
-                    print('nested data: ', nested_data)
                     script.append(event_insert(nested_data['claim_number'], data['event_type'], nested_data['supplier'], nested_data['cost']))
 
     return script
