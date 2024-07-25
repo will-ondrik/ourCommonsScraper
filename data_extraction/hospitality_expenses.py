777fdf89e1df9254e38c201e20d1603c31ed4422
Will Ondrik <wondrik@bcit.ca>
2024-07-25 09:11:22 -0700

    Added two functions:
    1) extract event type which:
    - takes the data from the event row and removes redundant data before returning a string containing the event type
    
    2) extract hospitality expenses function which:
    - takes a webdriver and href value as arguments
    - add the href value to the base url to generate the correct url for that specific MP
    - it uses the driver and beautiful soup to extract and parse the page's html content
    - iterates through the hospitality expenses and its nested data before adding each iterations data to a dictionary

---
+++ data_extraction/hospitality_expenses.py
@@ -0,0 +1,68 @@
+from bs4 import BeautifulSoup
+from constants.constants import BASE_URL, NO_TBODY_FOUND, EXPENSES_MAIN_INFO
+from data_extraction.utils import format_dollar_values
+import time
+
+def extract_event_type(event_row):
+    event_data = event_row.text.strip()
+    return event_data.replace('Type of Event', '').strip()
+
+def extract_hospitality_expenses(driver, href_value):
+    time.sleep(1)
+    hospitality_data = []
+    PAGE_URL = BASE_URL + href_value
+    driver.get(PAGE_URL)
+    time.sleep(1)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    
+    tbody = soup.find('tbody')
+    if not tbody:
+        print(NO_TBODY_FOUND)
+        return
+    
+    rows = tbody.find_all('tr')
+    i = 0
+    while i < len(rows):
+        row = rows[i]
+        if EXPENSES_MAIN_INFO in row.get('class', []):
+            row_data = row.find_all('td')
+            date = row_data[0].text.strip()
+            location = row_data[1].text.strip()
+            attendance = row_data[2].text.strip()
+            purpose = row_data[3].text.strip()
+            total = format_dollar_values(row_data[4].text.strip())
+
+            nested_row_data = []
+            nested_rows = rows[i + 1].find_all('tr')
+            event_row = rows[i + 1].find(class_='col-md-4')
+            event_type = extract_event_type(event_row)
+
+            for row in nested_rows[1:len(nested_rows)-1]:
+                row_data = row.find_all('td')
+                claim_number = row_data[0].text.strip()
+                supplier = row_data[1].text.strip()
+                cost = format_dollar_values(row_data[2].text.strip())
+
+                nested_row_data.append({
+                    'claim_number': claim_number,
+                    'supplier': supplier,
+                    'cost': cost
+                })
+
+            hospitality_data.append({
+                'event_type': event_type,
+                'date': date,
+                'location': location,
+                'attendance': attendance,
+                'purpose': purpose,
+                'total_cost': total,
+                'details': nested_row_data
+            })
+
+            i += 2
+        else:
+            i += 1
+
+    return hospitality_data
