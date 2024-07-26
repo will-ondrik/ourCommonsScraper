3ec3e0b11eaebffa494223f9dfea3d02d4908e12
Will Ondrik <wondrik@bcit.ca>
2024-07-25 19:02:18 -0700

    Corrected bug with null values in the expense insert.

--- data_processing/process_travel_expenses.py
+++ data_processing/process_travel_expenses.py
@@ -2,14 +2,14 @@ import data_processing.utils as utils
 from constants.constants import EMPTY_STRING
 
 def travel_claim_insert(start_date, end_date, transportation_cost, acccomodation_cost, meals_and_incidentals_cost, regular_points_used, special_points_used, usa_points_used, total_cost):
-    return f"INSERT INTO TravelClaim (expenseId, memberId, startDate, endDate, totalCost) VALUES (@expenseId, @mpId, '{start_date}', '{end_date}', {transportation_cost}, {acccomodation_cost}, {meals_and_incidentals_cost}, {regular_points_used}, {special_points_used}, {usa_points_used}, {total_cost});"
+    return f"INSERT INTO TravelClaim (expenseId, memberId, startDate, endDate, transportationCosts, accommodationCosts, mealsAndIncidentalsCosts, regularPoints, specialPoints, usaPoints,totalCost) VALUES (@expenseId, @mpId, '{start_date}', '{end_date}', {transportation_cost}, {acccomodation_cost}, {meals_and_incidentals_cost}, {regular_points_used}, {special_points_used}, {usa_points_used}, {total_cost});"
 
 
 def traveller_insert(first_name, last_name, traveller_type):
     return f"INSERT INTO Traveller (firstName, lastName, type) VALUES ('{first_name}', '{last_name}', '{traveller_type}');"
 
-def travel_insert(purpose, city='NULL', departure='NULL', destination='NULL', date='NULL'):
-    return f"INSERT INTO Travel (travellerId, claimId, date, departure, destination, purpose) VALUES (@travellerId, @claimId, '{date}', '{departure}', '{destination}', '{purpose}', '{city}');"
+def travel_insert(date, departure, destination, purpose):
+    return f"INSERT INTO Travel (travellerId, claimId, date, departure, destination, purpose) VALUES (@travellerId, @claimId, '{date}', '{departure}', '{destination}', '{purpose}');"
 
 def sequelize(travel_data, startPeriod, endPeriod, year, quarter):
     script = []
@@ -25,14 +25,12 @@ def sequelize(travel_data, startPeriod, endPeriod, year, quarter):
             return
         else:
             for nested_data in data['details']:
+                print('NESTED DATA: ', nested_data)
                 if nested_data.get('traveller_name')[0] == EMPTY_STRING:
                     script.append(traveller_insert(nested_data['traveller_name'][0].strip(), nested_data['traveller_name'][0], nested_data['traveller_type']))
                 else:
                     script.append(traveller_insert(nested_data['traveller_name'][1].strip(), nested_data['traveller_name'][0], nested_data['traveller_type']))
                 script.append(utils.set_var_traveller_id())
-                if nested_data.get('travel_date') is None and nested_data.get('departure') is None:
-                    script.append(travel_insert(nested_data['purpose_of_travel'], nested_data['destination'], departure='NULL', destination='NULL', date='NULL'))
-                else:
-                    script.append(travel_insert(nested_data['travel_date'], nested_data['departure'], nested_data['destination'], nested_data['purpose_of_travel']))
+                script.append(travel_insert(nested_data['travel_date'], nested_data['departure'], nested_data['destination'], nested_data['purpose_of_travel']))
 
     return script     
