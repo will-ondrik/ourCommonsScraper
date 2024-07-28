9ac26802552bb32a860b95c9c6ad817bbd52bac4
Will Ondrik <wondrik@bcit.ca>
2024-07-28 16:51:59 -0700

    Added functions to correctly format the dollar values.

--- data_extraction/mp_data.py
+++ data_extraction/mp_data.py
@@ -14,12 +14,18 @@ def extract_mp_data(row_data):
     constituency = row_data[1].text.strip()
     caucus = row_data[2].text.strip()
     salary = format_dollar_values(row_data[3].text.strip())
+    travel_expenses = format_dollar_values(row_data[4].text.strip())
+    hospitality_expenses = format_dollar_values(row_data[5].text.strip())
+    contract_expenses = format_dollar_values(row_data[6].text.strip())
     mp_data.append({
         'first_name': first_name,
         'last_name': last_name,
         'constituency': constituency,
         'caucus': caucus,
-        'salary': salary
+        'salary': salary,
+        'travel_expenses': travel_expenses,
+        'hospitality_expenses': hospitality_expenses,
+        'contract_expenses': contract_expenses
     })
 
     return mp_data
